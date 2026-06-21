a0bca7deba61f3f4179371e21c7980a7f3de4bfe
hmqaal <46423638+hmqaal@users.noreply.github.com>
2026-06-21 09:21:02 +0100

    Change scatterplot layer radius to a smaller value

--- app/page.tsx
+++ app/page.tsx
@@ -287,7 +287,7 @@ export default function Home() {
       id: "settlements",
       data: filtered,
       getPosition: (d: Settlement) => [Number(d.X), Number(d.Y)],
-      getRadius:4,
+      getRadius:0.01,
       radiusUnits: "pixels",
       getFillColor: (d: Settlement) => colorForName(dominantTribe(Number(d.OBJECTID))),
       getLineColor: [255, 255, 255],
